9e642a9680599e6bc5f12f590929bf5c572da6e7
trung2515 <dinhtrung2515@gmail.com>
2023-12-08 17:19:16 +0700

    seeder employee

--- server/src/seeders/create-employee.js
+++ server/src/seeders/create-employee.js
@@ -1,31 +1,36 @@
 'use strict';
-const dataMock = []
-// for(const i = 5; i <100 ; i++){
-  
-//   const item =  {
-//         employee_id:'AD'+ i.padStart(4,'0'),
-//         full_name: 'Nguyễn văn,
-//         first_name:'Tâm',
-//         phone:'0123445556',
-//         email:'tamtm@gmail.com',
-//         gender:""
-//         department_id	:'PBNS',
-//         position_id:'CVLD',
-//         deleted:'0',
-//         createdAt: new Date('10-10-2023'),
-//         createdAt: new Date('10-10-2023')
-//       },
-// }
+let dataMock = []
+for(let i = 5; i <100 ; i++){
+  let number = Math.floor(Math.random() * 10)
+  let random = number % 2
+  let item =  {
+        employee_id:'AD'+ i.toString().padStart(4,'0'),
+        full_name: (random == 0 ? 'Nguyễn Văn Số '  : 'Trần Thị Nữ Số ') + i,
+        first_name:'Số' + i,
+        phone:'0123445556',
+        email:'email@gmail.com',
+        gender: random == 0 ? 'male' :'female',
+        dayOfBirth:new Date('05-05-1990'),
+        department_id	:'PBKD',
+        position_id:'CVMB',
+        deleted:'0',
+        createdAt: new Date('10-10-2023'),
+        createdAt: new Date('10-10-2023')
+      }
+      dataMock.push(item)
+}
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Positions', [
+    await queryInterface.bulkInsert('Employees', [
       {
         employee_id:'AD0001',
         full_name: 'Trương Minh Tâm',
         first_name:'Tâm',
         phone:'0123445556',
         email:'tamtm@gmail.com',
+        gender:'male',
+        dayOfBirth:new Date('05-05-1990'),
         department_id	:'PBNS',
         position_id:'CVLD',
         deleted:'0',
@@ -38,6 +43,8 @@ module.exports = {
         first_name:'Thông',
         phone:'0925578467',
         email:'thongtha@gmail.com',
+        gender:'male',
+        dayOfBirth:new Date('05-05-1990'),
         department_id	:'PBNS',
         position_id:'CVMB',
         deleted:'0',
@@ -50,6 +57,8 @@ module.exports = {
         first_name:'Vũ',
         phone:'0897784876',
         email:'vudh@gmail.com',
+        gender:'male',
+        dayOfBirth:new Date('05-05-1990'),
         department_id	:'PBNS',
         position_id:'CVMB',
         deleted:'0',
@@ -62,6 +71,8 @@ module.exports = {
         first_name:'Trung',
         phone:'0425598648',
         email:'trungpd@gmail.com',
+        gender:'male',
+        dayOfBirth:new Date('05-05-1990'),
         department_id	:'PBNS',
         position_id:'CVMB',
         deleted:'0',
@@ -71,10 +82,10 @@ module.exports = {
 
 
 
-    ], {});
+    ].concat(dataMock), {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Positions', null, {});
+    await queryInterface.bulkDelete('Employees', null, {});
   }
 };
